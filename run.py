c24cd9817559fabf37fc935190761294271d7575
BijoyKM <bijoy.k.mathew@outlook.com>
2022-10-25 08:03:17 +0000

    Add function to validate entered  customer name

--- run.py
+++ run.py
@@ -13,10 +13,25 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('coffeehousePos')
 
 
+def validate_cust_name(value):
+    """
+    Function to validate if entered name is all characters
+    """
+    if value.isalpha():
+        return True
+
+
 def get_cust_name():
+    """
+    Function for getting customer name
+    """
     print("*****Welcome to Coffeehouse!!*****\n")
     cust_name = input("Please enter your name:")
-    print(cust_name)
+    if validate_cust_name(cust_name):
+        print(f"Hi {cust_name}, Your order number: \n")
+        print("Please provide this at the counter when paying for the order.")
+    else:
+        print("Please enter a valid name. Thanks")
 
 
 get_cust_name()
