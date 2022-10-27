2cdee7284980c45aa154d37fa06fd9f919cfea3b
BijoyKM <bijoy.k.mathew@outlook.com>
2022-10-27 11:08:46 +0000

    Add function to clear the terminal

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
-import gspread
-from google.oauth2.service_account import Credentials
 import random
 import string
+from os import system, name
+import gspread
+from google.oauth2.service_account import Credentials
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -26,10 +27,13 @@ def validate_cust_name(value):
 
 
 def create_order_num(chars=string.ascii_uppercase + string.digits, N=5):
-    return ''.join(random.choice(chars) for i in range (N))
+    """
+    Function to create alphanumeric order number of 5 character length
+    """
+    return ''.join(random.choice(chars) for i in range(N))
 
 
-order_num = create_order_num()
+ORDER_NUM = create_order_num()
 
 
 def get_cust_name():
@@ -37,14 +41,26 @@ def get_cust_name():
     Function for getting customer name
     """
     print("*****Welcome to Coffeehouse!!*****\n")
-    print("Please provide your name. It should be in between 2 to 25 characters long.")
+    print("Please provide your name."
+          + "It should be in between 2 to 25 characters long.")
     cust_name = input("Please enter your name: ")
     if validate_cust_name(cust_name):
         print(f"Hello {cust_name}")
-        print(f"Your order number is: {order_num}")
+        print(f"Your order number is: {ORDER_NUM}")
         print("Please provide this at the counter when paying for the order.")
     else:
         print("Please enter a valid name. Thanks")
 
 
+def clear_screen():
+    """
+    Function to clear the terminal when switching screen
+    """
+    if name == 'nt':
+        system('cls')
+    else:
+        system('clear')
+
+
 get_cust_name()
+clear_screen()
