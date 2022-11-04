bf28fddaf886aed5ec9a9349bbfbc25954ca63d5
BijoyKM <bijoy.k.mathew@outlook.com>
2022-11-04 22:52:02 +0000

    Add comments to code

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # CoffeehousePOS
 
-![Coffeehousepos Welcome Screen Image](/assets/images/readmeimages/WelcomeScreen.jpg "Welcome Screen Image")
+
+![CoffeehousePOS](/assets/images/readmeimages/WelcomeScreen.jpg "Welcome screen image")
 
 ## Live site can be viewed here. 
 # [Coffeehousepos](https://coffeehousepos.herokuapp.com/)
 
 ## Repository
-## https://github.com/BijoyKM/coffeehousePos
+## [https://github.com/BijoyKM/coffeehousePos](https://github.com/BijoyKM/coffeehousePos)
 
 
 
@@ -18,3 +19,13 @@
 - [Deployment](#deployment)
 - [Credits](#credits)
 - [Acknowledgments](#acknowledgments)
+
+
+# User Experience (UX)
+## User Stories
+* ## As a Website Owner I want that:
+1. The website provides clear and concise information about the organic farm business, the produce with prices and the farm trip information.
+2. The website allows customers to contact the business for placing orders, booking farm trips or providing feedback.
+3. The website provides an online platform for new and old customers to connect with Farm Organiks which in turn would increase the walk-ins.
+
+[def]: /assets/images/readmeimages/WelcomeScreen.jpg "Welcome Screen Image"
--- run.py
+++ run.py
@@ -11,6 +11,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
+# gspreads constants
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
@@ -36,6 +37,7 @@ def create_order_num(chars=string.ascii_uppercase + string.digits, length=5):
     return ''.join(random.choice(chars) for i in range(length))
 
 
+# constants
 ORDER_NUM = create_order_num()
 ORDER_TOTAL = None
 
@@ -88,10 +90,13 @@ def get_cust_name():
         print("Please provide your name.")
         print("It should be in between 2 to 25 characters long.\n\n")
         cust_name = input("Please enter your name: \n")
+        # validates customer name
         if validate_cust_name(cust_name):
             print(f"Hello {cust_name}")
             order_worksheet = SHEET.worksheet("order")
+            # update order value in order sheet
             order_worksheet.update_cell(2, 1, ORDER_NUM)
+            # update customer name in order sheet
             order_worksheet.update_cell(2, 2, cust_name)
             print(f"Your order value is: {ORDER_NUM}")
             print("Please keep note of this value if you would like to search")
@@ -124,17 +129,24 @@ def update_sales_sheet():
     and clears order sheet
     """
     order_worksheet = SHEET.worksheet("order")
+    # copies each order row values to sales sheet
     sales_value_row = order_worksheet.row_values(2)
+    # clears order sheet values
     order_worksheet.delete_rows(2)
     sales_worksheet = SHEET.worksheet("sales")
     sales_worksheet.append_row(sales_value_row)
 
 
 def pay_by_card():
+    """
+    function to get card digits and pin, also validates input values
+    """
+    # list to save card digits entered
     pay_det = []
     print("Please insert your 16 digit card number")
     while True:
         value = input("Enter Here: ")
+        # validates input card number to be integer and is of 16 digits
         if value.isdigit() and len(value) == 16:
             pay_det.append(value)
             break
@@ -144,6 +156,7 @@ def pay_by_card():
     print("Please enter your Four digit pin.")
     while True:
         ent_value = input("Enter Here: ")
+        # validates input pin to be integer and is of 4 digits
         if ent_value.isdigit() and len(ent_value) == 4:
             print("Your payment is successful.")
             break
@@ -158,27 +171,35 @@ def coffee_screen():
     """
     clear_screen()
     item_price = None
+    # header
     print("        Coffee Screen")
     print('********************************')
     coffee_cup_ascii()
+    # saving all values of coffee sheet to coffee_data
     coffee_data = SHEET.worksheet('coffee').get_all_values()
+    # saving item row to coffee_item_row
     coffee_item_row = coffee_data[0]
+    # saving price row to coffee_price_row
     coffee_price_row = coffee_data[-1]
     coffee_data = SHEET.worksheet('coffee')
     order_worksheet = SHEET.worksheet("order")
     item_num = 1
+    # printing items name with their prices
     for item_name, price in zip(coffee_item_row, coffee_price_row):
         print(f"{item_num}: {item_name} ============= € {price}\n")
         item_num += 1
     print("7: Back to Order Screen")
+    # depending upon user input excecutes code
     while True:
         choice = input("Enter Choice. Input number between 1 and 7: \n")
         if choice == '1':
             coffee_data = SHEET.worksheet('coffee')
             order_worksheet = SHEET.worksheet("order")
             item_price = coffee_data.cell('2', 1).value
+            # updating order sheet item price cell with chosen item price
             order_worksheet.update_cell(2, 3, item_price)
             item_value = 1
+            # updating order sheet item name with value 1
             order_worksheet.update_cell(2, 4, item_value)
             print(f"Your order#: {ORDER_NUM}")
             print(f"{item_value} {coffee_item_row[0]} Total: €{item_price}")
@@ -187,8 +208,10 @@ def coffee_screen():
             coffee_data = SHEET.worksheet('coffee')
             order_worksheet = SHEET.worksheet("order")
             item_price = coffee_data.cell('2', 2).value
+            # updating order sheet item price cell with chosen item price
             order_worksheet.update_cell(2, 3, item_price)
             item_value = 1
+            # updating order sheet item name with value 1
             order_worksheet.update_cell(2, 5, item_value)
             print(f"Your order#: {ORDER_NUM}")
             print(f"{item_value} {coffee_item_row[1]} Total: €{item_price}")
@@ -197,8 +220,10 @@ def coffee_screen():
             coffee_data = SHEET.worksheet('coffee')
             order_worksheet = SHEET.worksheet("order")
             item_price = coffee_data.cell('2', 3).value
+            # updating order sheet item price cell with chosen item price
             order_worksheet.update_cell(2, 3, item_price)
             item_value = 1
+            # updating order sheet item name with value 1
             order_worksheet.update_cell(2, 6, item_value)
             print(f"Your order#: {ORDER_NUM}")
             print(f"{item_value} {coffee_item_row[2]} Total: €{item_price}")
@@ -207,8 +232,10 @@ def coffee_screen():
             coffee_data = SHEET.worksheet('coffee')
             order_worksheet = SHEET.worksheet("order")
             item_price = coffee_data.cell('2', 4).value
+            # updating order sheet item price cell with chosen item price
             order_worksheet.update_cell(2, 3, item_price)
             item_value = 1
+            # updating order sheet item name with value 1
             order_worksheet.update_cell(2, 7, item_value)
             print(f"Your order#: {ORDER_NUM}")
             print(f"{item_value} {coffee_item_row[3]} Total: €{item_price}")
@@ -217,8 +244,10 @@ def coffee_screen():
             coffee_data = SHEET.worksheet('coffee')
             order_worksheet = SHEET.worksheet("order")
             item_price = coffee_data.cell('2', 5).value
+            # updating order sheet item price cell with chosen item price
             order_worksheet.update_cell(2, 3, item_price)
             item_value = 1
+            # updating order sheet item name with value 1
             order_worksheet.update_cell(2, 8, item_value)
             print(f"Your order#: {ORDER_NUM}")
             print(f"{item_value} {coffee_item_row[4]} Total: €{item_price}")
@@ -227,8 +256,10 @@ def coffee_screen():
             coffee_data = SHEET.worksheet('coffee')
             order_worksheet = SHEET.worksheet("order")
             item_price = coffee_data.cell('2', 6).value
+            # updating order sheet item price cell with chosen item price
             order_worksheet.update_cell(2, 3, item_price)
             item_value = 1
+            # updating order sheet item name with value 1
             order_worksheet.update_cell(2, 9, item_value)
             print(f"Your order#: {ORDER_NUM}")
             print(f"{item_value} {coffee_item_row[5]} Total: €{item_price}")
@@ -254,27 +285,35 @@ def tea_screen():
     Function providing the tea screen
     """
     clear_screen()
+    # header
     print("          Tea Screen")
     print('********************************')
     tea_kettle_ascii()
+    # saving all values of tea sheet to tea_data
     tea_data = SHEET.worksheet('tea').get_all_values()
+    # saving item row to tea_item_row
     tea_item_row = tea_data[0]
+    # saving price row to tea_price_row
     tea_price_row = tea_data[-1]
     tea_data = SHEET.worksheet('tea')
     order_worksheet = SHEET.worksheet("order")
     item_num = 1
+    # printing items name with their prices
     for item_name, price in zip(tea_item_row, tea_price_row):
         print(f"{item_num}: {item_name} ============= € {price}\n")
         item_num += 1
     print("7: Back to Order Screen")
+    # depending upon user input excecutes code
     while True:
         choice = input("Enter Choice. Input number between 1 and 7: \n")
         if choice == '1':
             tea_data = SHEET.worksheet('tea')
             order_worksheet = SHEET.worksheet("order")
             item_price = tea_data.cell('2', 1).value
+            # updating order sheet item price cell with chosen item price
             order_worksheet.update_cell(2, 3, item_price)
             item_value = 1
+            # updating order sheet item name with value 1
             order_worksheet.update_cell(2, 10, item_value)
             print(f"Your order#: {ORDER_NUM}")
             print(f"{item_value} {tea_item_row[0]} Total: €{item_price}")
@@ -283,8 +322,10 @@ def tea_screen():
             tea_data = SHEET.worksheet('tea')
             order_worksheet = SHEET.worksheet("order")
             item_price = tea_data.cell('2', 2).value
+            # updating order sheet item price cell with chosen item price
             order_worksheet.update_cell(2, 3, item_price)
             item_value = 1
+            # updating order sheet item name with value 1
             order_worksheet.update_cell(2, 11, item_value)
             print(f"Your order#: {ORDER_NUM}")
             print(f"{item_value} {tea_item_row[1]} Total: €{item_price}")
@@ -293,8 +334,10 @@ def tea_screen():
             tea_data = SHEET.worksheet('tea')
             order_worksheet = SHEET.worksheet("order")
             item_price = tea_data.cell('2', 3).value
+            # updating order sheet item price cell with chosen item price
             order_worksheet.update_cell(2, 3, item_price)
             item_value = 1
+            # updating order sheet item name with value 1
             order_worksheet.update_cell(2, 12, item_value)
             print(f"Your order#: {ORDER_NUM}")
             print(f"{item_value} {tea_item_row[2]} Total: €{item_price}")
@@ -303,8 +346,10 @@ def tea_screen():
             tea_data = SHEET.worksheet('tea')
             order_worksheet = SHEET.worksheet("order")
             item_price = tea_data.cell('2', 4).value
+            # updating order sheet item price cell with chosen item price
             order_worksheet.update_cell(2, 3, item_price)
             item_value = 1
+            # updating order sheet item name with value 1
             order_worksheet.update_cell(2, 13, item_value)
             print(f"Your order#: {ORDER_NUM}")
             print(f"{item_value} {tea_item_row[3]} Total: €{item_price}")
@@ -313,8 +358,10 @@ def tea_screen():
             tea_data = SHEET.worksheet('tea')
             order_worksheet = SHEET.worksheet("order")
             item_price = tea_data.cell('2', 5).value
+            # updating order sheet item price cell with chosen item price
             order_worksheet.update_cell(2, 3, item_price)
             item_value = 1
+            # updating order sheet item name with value 1
             order_worksheet.update_cell(2, 14, item_value)
             print(f"Your order#: {ORDER_NUM}")
             print(f"{item_value} {tea_item_row[4]} Total: €{item_price}")
@@ -323,8 +370,10 @@ def tea_screen():
             tea_data = SHEET.worksheet('tea')
             order_worksheet = SHEET.worksheet("order")
             item_price = tea_data.cell('2', 6).value
+            # updating order sheet item price cell with chosen item price
             order_worksheet.update_cell(2, 3, item_price)
             item_value = 1
+            # updating order sheet item name with value 1
             order_worksheet.update_cell(2, 15, item_value)
             print(f"Your order#: {ORDER_NUM}")
             print(f"{item_value} {tea_item_row[5]} Total: €{item_price}")
@@ -350,27 +399,35 @@ def desserts_screen():
     Function providing the desserts screen
     """
     clear_screen()
+    # header
     print("       Desserts Screen")
     print('********************************')
     dessert_ascii()
+    # saving all values of tea sheet to desserts_data
     desserts_data = SHEET.worksheet('desserts').get_all_values()
+    # saving item row to desserts_item_row
     desserts_item_row = desserts_data[0]
+    # saving price row to desserts_price_row
     desserts_price_row = desserts_data[-1]
     desserts_data = SHEET.worksheet('desserts')
     order_worksheet = SHEET.worksheet("order")
     item_num = 1
+    # printing items name with their prices
     for item_name, price in zip(desserts_item_row, desserts_price_row):
         print(f"{item_num}: {item_name} ============= € {price}\n")
         item_num += 1
     print("7: Back to Order Screen")
+    # depending upon user input excecutes code
     while True:
         choice = input("Enter Choice. Input number between 1 and 7: \n")
         if choice == '1':
             desserts_data = SHEET.worksheet('desserts')
             order_worksheet = SHEET.worksheet("order")
             item_price = desserts_data.cell('2', 1).value
+            # updating order sheet item price cell with chosen item price
             order_worksheet.update_cell(2, 3, item_price)
             item_value = 1
+            # updating order sheet item name with value 1
             order_worksheet.update_cell(2, 16, item_value)
             print(f"Your order#: {ORDER_NUM}")
             print(f"{item_value} {desserts_item_row[0]} Total: €{item_price}")
@@ -379,8 +436,10 @@ def desserts_screen():
             desserts_data = SHEET.worksheet('desserts')
             order_worksheet = SHEET.worksheet("order")
             item_price = desserts_data.cell('2', 2).value
+            # updating order sheet item price cell with chosen item price
             order_worksheet.update_cell(2, 3, item_price)
             item_value = 1
+            # updating order sheet item name with value 1
             order_worksheet.update_cell(2, 17, item_value)
             print(f"Your order#: {ORDER_NUM}")
             print(f"{item_value} {desserts_item_row[1]} Total: €{item_price}")
@@ -389,8 +448,10 @@ def desserts_screen():
             desserts_data = SHEET.worksheet('desserts')
             order_worksheet = SHEET.worksheet("order")
             item_price = desserts_data.cell('2', 3).value
+            # updating order sheet item price cell with chosen item price
             order_worksheet.update_cell(2, 3, item_price)
             item_value = 1
+            # updating order sheet item name with value 1
             order_worksheet.update_cell(2, 18, item_value)
             print(f"Your order#: {ORDER_NUM}")
             print(f"{item_value} {desserts_item_row[2]} Total: €{item_price}")
@@ -399,8 +460,10 @@ def desserts_screen():
             desserts_data = SHEET.worksheet('desserts')
             order_worksheet = SHEET.worksheet("order")
             item_price = desserts_data.cell('2', 4).value
+            # updating order sheet item price cell with chosen item price
             order_worksheet.update_cell(2, 3, item_price)
             item_value = 1
+            # updating order sheet item name with value 1
             order_worksheet.update_cell(2, 19, item_value)
             print(f"Your order#: {ORDER_NUM}")
             print(f"{item_value} {desserts_item_row[3]} Total: €{item_price}")
@@ -409,8 +472,10 @@ def desserts_screen():
             desserts_data = SHEET.worksheet('desserts')
             order_worksheet = SHEET.worksheet("order")
             item_price = desserts_data.cell('2', 5).value
+            # updating order sheet item price cell with chosen item price
             order_worksheet.update_cell(2, 3, item_price)
             item_value = 1
+            # updating order sheet item name with value 1
             order_worksheet.update_cell(2, 20, item_value)
             print(f"Your order#: {ORDER_NUM}")
             print(f"{item_value} {desserts_item_row[4]} Total: €{item_price}")
@@ -419,8 +484,10 @@ def desserts_screen():
             desserts_data = SHEET.worksheet('desserts')
             order_worksheet = SHEET.worksheet("order")
             item_price = desserts_data.cell('2', 6).value
+            # updating order sheet item price cell with chosen item price
             order_worksheet.update_cell(2, 3, item_price)
             item_value = 1
+            # updating order sheet item name with value 1
             order_worksheet.update_cell(2, 21, item_value)
             print(f"Your order#: {ORDER_NUM}")
             print(f"{item_value} {desserts_item_row[5]} Total: €{item_price}")
@@ -446,24 +513,40 @@ def get_sales():
     """
     sales_data = SHEET.worksheet("sales")
     input_order_num = input("Please enter your order value in UPPERCASE: ")
+    # saving ORDER_NUM column data of sales sheet to a list
     order_value_list = sales_data.col_values(1)
+    # deleting ORDER_NUM value from the list
     del order_value_list[0]
+    # finding if entered order value is in sales sheet
+    # this returns None if entered order value is not in sales sheet
+    # if order value is there then returns row and colume of that cell
     order_num_cell = sales_data.find(input_order_num)
     if order_num_cell is None:
         print(f"{input_order_num} is not a Valid order value"
               + " as no purchases are registered against it yet.")
+    # checks if entered order value is not in ORDER_NUM column in sales sheet
     if input_order_num not in order_value_list:
         print(f"{input_order_num} is not a Valid order value"
               + " as no purchases are registered against it yet.")
     else:
+        # saves row number
         row_num = order_num_cell.row
+        # gets customer name column
         cust_col = (order_num_cell).col + 1
+        # gets customer value from cell
         customer = sales_data.cell(row_num, cust_col).value
+        # gets order price column
         order_val_col = cust_col + 1
+        # gets order price value from cell
         order_val = sales_data.cell(row_num, order_val_col).value
+        # gets headings row from sales sheet
         name_row = sales_data.row_values(1)
+        # gets order details row from sales sheet
         item_row = sales_data.row_values(row_num)
+        # list to store headings from sales sheet
         name_list = []
+        # loop to find if order details row cell and heading row cell have data
+        # order detail cell will have item_value set to 1
         for col_name, col_num in zip(name_row, item_row):
             if col_name and col_num:
                 last_item = col_name
@@ -471,6 +554,7 @@ def get_sales():
         print("Your search details are:\n")
         print(f"ORDER VALUE: {input_order_num}")
         print(f"Customer Name : {customer}")
+        # third item on name_list will heading which has item_value set to 1
         print(f"Order : {name_list[3]}")
         print(f"Price: €{order_val}")
         print('********************************')
@@ -483,6 +567,7 @@ def order_screen():
     Function providing the order screen
     """
     while True:
+        # header
         print('********************************')
         print("Order Screen")
         print('********************************')
